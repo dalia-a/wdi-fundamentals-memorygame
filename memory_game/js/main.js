1023b3a040f634b8d850c7864f1b87a1cda7223d
Dalia <dalia.91a@gmail.com>
2019-09-22 13:54:48 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,73 @@
-var cards = ["queen","queen","king","king"];
+// create array of cards
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+// create empty array to store cards
 var cardsInPlay = [];
-var cardOne = cards [0];
-cardsInPlay.push(cardOne);
-console.log("User flipped" + cardOne);
-var cardTwo = cards [1];
-cardsInPlay.push(cardTwo);
-console.log("user flipped" + cardTwo);
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match!");
-    } else {
-      ("Sorry, try again.");
-    }
-  }
+
+var flipCard = function () {
+	// get the attribute data-id from cardElement and store it in variable cardId. cardId = data-id.
+	var cardId = this.getAttribute('data-id');
+	// log user flipped and using cardId to reference cards array and referencing the key .rank.
+	console.log("User flipped " + cards[cardId].rank);
+	// pushing the string, the value of rank, into the cardInPlay array
+	cardsInPlay.push(cards[cardId].rank);
+	// displaying image and suit
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	// this = img, this sets the source attribute of the card that has been clicked, and puts the card image in 
+	this.setAttribute('src', cards[cardId].cardImage);
+	
+		if (cardsInPlay.length === 2) {checkForMatch(cardId); }
+}
 
 
+var checkForMatch = function() {
+	// creates condition if the first card flipped is the same as the second card that is flipped create alert
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	// if the cards are not the same run the below alert
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+// create DOM elements from cards array 
+var createBoard = function () {
+	//creating a loop on all cards
+	for (var i = 0; i < cards.length; i++) {
+		// creating a img element and storing in in variable cardElement
+		var cardElement = document.createElement('img');
+		// sets back of card img source to png file
+		cardElement.setAttribute('src', "images/back.png");
+		// giving each card a data id, starting at 0
+		cardElement.setAttribute('data-id', i);
+		// adding to each card an event listener, on click run flipCard function
+		cardElement.addEventListener('click', flipCard);
+		// find the element of Id 'game-board' and then adding cardElement to the game-board element
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+// calling the createBoard function
+createBoard();
