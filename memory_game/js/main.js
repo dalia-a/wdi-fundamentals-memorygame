2bcf64d84584654e890466531f18cb71e7b141d5
Dalia <dalia.91a@gmail.com>
2019-09-22 13:09:11 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,17 @@
-console.log("Up and running!");
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
-console.log("User flipped " + cardOne);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards [0];
+cardsInPlay.push(cardOne);
+console.log("User flipped" + cardOne);
+var cardTwo = cards [1];
+cardsInPlay.push(cardTwo);
+console.log("user flipped" + cardTwo);
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      ("Sorry, try again.");
+    }
+  }
+
+
